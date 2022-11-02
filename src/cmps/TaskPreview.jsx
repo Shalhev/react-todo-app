89c2ff246aee91fe5a61f43b041a0b43657ddaa7
Shalhev Nagauker <shalhav96@gmail.com>
2022-11-02 18:19:08 +0200

    add task preview style

--- src/cmps/TaskPreview.jsx
+++ src/cmps/TaskPreview.jsx
@@ -1,11 +1,11 @@
 export function TaskPreview({ task, onRemoveTask, onCompleteTask }) {
     return (
-        <div className="task-preview flex auto-center" >
+        <div className="task-preview" >
             <div className="round">
                 <input type="checkbox" id="checkbox" defaultChecked={task.isComplete} onChange={() => onCompleteTask(task)} />
             </div>
             <p className={task.isComplete ? 'complete' : ''}>{task.txt}</p>
-            < button onClick={() => onRemoveTask(task.id)}>X</button>
+            <div className="remove-task" onClick={() => onRemoveTask(task.id)}>✕</div>
         </div>
     )
 }
