c789848c9f6aa67ab02007c4616a03422db374e7
Shalhev Nagauker <shalhav96@gmail.com>
2022-11-04 00:43:07 +0200

    check btn style

--- src/cmps/TaskPreview.jsx
+++ src/cmps/TaskPreview.jsx
@@ -1,8 +1,9 @@
 export function TaskPreview({ task, onRemoveTask, onCompleteTask }) {
     return (
         <div className="task-preview" >
-            <div className="round flex auto-center">
-                <input type="checkbox" id="checkbox" defaultChecked={task.isComplete} onChange={() => onCompleteTask(task)} />
+            <div className="outter-circle flex auto-center">
+                {/* <input type="checkbox" id="checkbox" defaultChecked={task.isComplete} onChange={() => onCompleteTask(task)} /> */}
+                <div onClick={() => onCompleteTask(task)} className={task.isComplete ? 'circle check' : 'circle'}></div>
             </div>
             <p className={task.isComplete ? 'complete' : ''}>{task.txt}</p>
             <div className="remove-task" onClick={() => onRemoveTask(task.id)}>✕</div>
